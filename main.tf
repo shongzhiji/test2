d0bf7be4f2650476b4deffd5a83169b7cb2c77d9
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 22:56:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,30 @@ resource "alicloud_security_group" "default" {
   name = "default"
   vpc_id = alicloud_vpc.vpc.id
 }
-
+module "tf-instances" {  
+ source                      = "alibaba/ecs-instance/alicloud"  
+ region                      = "cn-hangzhou-i"  
+ number_of_instances         = var.instance_num  
+ vswitch_id                  = alicloud_vswitch.vsw.id  
+ group_ids                   = [alicloud_security_group.default.id]  
+ private_ips                 = var.private_ips
+ image_ids                   = ["ubuntu_18_04_64_20G_alibase_20190624.vhd"]  
+ instance_type               = var.instance_type  
+ internet_max_bandwidth_out  = 10
+ associate_public_ip_address = true  
+ instance_name               = "my_module_instances_"  
+ host_name                   = "sample"  
+ internet_charge_type        = "PayByTraffic"   
+ password                    = "User@123" 
+ system_disk_category        = "cloud_ssd"  
+ data_disks = [    
+  {      
+    disk_category = "cloud_ssd"      
+    disk_name     = "my_module_disk"      
+    disk_size     = "50"    
+  } 
+ ]
+}
 resource "alicloud_instance" "instance" {
   # cn-beijing
   availability_zone = "cn-hangzhou-i"
