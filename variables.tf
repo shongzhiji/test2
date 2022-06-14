e42007708b003e195c30d998627e188ea940792c
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 22:38:04 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "instance_number" {
+  default = 1
+  description = "1"
+}
+
+variable "instance_type" {
+  default = "ecs.n2.small"
+  description = "ecs.n2.small"
+}
+
+variable "instance_name" {
+  default = "tf_ecs_test"
+  description = "loong"
+}
