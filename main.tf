f4395b68c9139a92ba6457aac12f2eb4164d9c8c
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 23:23:31 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,29 +26,35 @@ resource "alicloud_security_group" "default" {
   name = "default"
   vpc_id = alicloud_vpc.vpc.id
 }
-module "tf-instances" {  
- source                      = "alibaba/ecs-instance/alicloud"  
- region                      = "cn-hangzhou-i"  
- number_of_instances         = var.instance_num  
- vswitch_id                  = alicloud_vswitch.vsw.id  
- group_ids                   = [alicloud_security_group.default.id]  
- private_ips                 = var.private_ips
- image_ids                   = ["ubuntu_18_04_64_20G_alibase_20190624.vhd"]  
- instance_type               = var.instance_type  
- internet_max_bandwidth_out  = 10
- associate_public_ip_address = true  
- instance_name               = "my_module_instances_"  
- host_name                   = "sample"  
- internet_charge_type        = "PayByTraffic"   
- password                    = "User@123" 
- system_disk_category        = "cloud_ssd"  
- data_disks = [    
-  {      
-    disk_category = "cloud_ssd"      
-    disk_name     = "my_module_disk"      
-    disk_size     = "50"    
-  } 
- ]
+
+data "alicloud_images" "ubuntu" {
+  most_recent = true
+  name_regex  = "^ubuntu_18.*64"
+}
+
+module "ecs_cluster" {
+  source  = "alibaba/ecs-instance/alicloud"
+
+  number_of_instances = var.instance_num  
+
+  name                        = "my-ecs-cluster"
+  use_num_suffix              = true
+  image_id                    = data.alicloud_images.ubuntu.ids.0
+  instance_type               = "ecs.sn1ne.large"
+  vswitch_id                  = "vsw-fhuqie"
+  security_group_ids          = ["sg-12345678"]
+  associate_public_ip_address = true
+  internet_max_bandwidth_out  = 10
+
+  key_name = "for-ecs-cluster"
+
+  system_disk_category = "cloud_ssd"
+  system_disk_size     = 50
+
+  tags = {
+    Created      = "Terraform"
+    Environment = "dev"
+  }
 }
 
 resource "alicloud_security_group_rule" "allow_all_tcp" {
