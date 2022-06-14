9a59c12fe92c670f5ebbcb863e002e930259a285
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 23:10:02 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "instance_num" {
-  default = 3
+  type        = number
+  default     = 3
   description = "3"
 }
 
 variable "instance_type" {
+  type        = string
   default = "ecs.n2.small"
   description = "ecs.n2.small"
 }
 variable "private_ips" {
+  type        = list(string)
   default = ["172.16.0.10", "172.16.0.11", "172.16.0.12"]  
   description = "collection"
 }
